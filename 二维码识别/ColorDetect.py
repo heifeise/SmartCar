4ee9df6f1d6fbf306654ea014ac3fd5671e55873
李易洋 <YiYang.Li.11Pro@outlook.com>
2022-11-03 02:23:30 +0800

    重构：更新ColorDetect，重构代码，改善部分逻辑

--- 二维码识别/ColorDetect.py
+++ 二维码识别/ColorDetect.py
@@ -8,21 +8,36 @@ def decode_display(image_input):  # 二维码裁剪
 
     if barcodes:
         print(barcodes)
+
         for barcode in barcodes:
+
             # 提取二维码的边界框的位置
             # 画出图像中条形码的边界框
             (x, y, w, h) = barcode.rect
-            cv2.rectangle(image_input, (x, y), (x + w, y + h), (225, 225, 225), 2)
+            cv2.rectangle(image_input,
+                          (x, y),
+                          (x + w, y + h),
+                          (225, 225, 225),
+                          2)
+
             # 提取二维码数据为字节对象，所以如果我们想在输出图像上
             # 画出来，就需要先将它转换成字符串
             ROI = image_input[y:y + h, x:x + h].copy()
-            barcodeData = barcode.data.decode("utf-8")
-            barcodeType = barcode.type
+            barcode_data = barcode.data.decode("utf-8")
+            barcode_type = barcode.type
+
             # 绘出图像上条形码的数据和条形码类型
-            text = "{} ({})".format(barcodeData, barcodeType)
-            # cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (225, 225, 225), 2)
+
+            text = f"{barcode_data} ({barcode_type})"
+            cv2.putText(image,
+                        text,
+                        (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (225, 225, 225),
+                        2)
             # 向终端打印条形码数据和条形码类型
-            # print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+            print(f"[信息] 识别到 {text}")
         return ROI
 
     return None
@@ -52,23 +67,27 @@ def color_detect(frame_input):
         color_result = "BLUE"
     elif 125 < hue_value < 155:
         color_result = "purple"
-    else:
-        pass
+
     return color_result
 
-
+# 单模块测试
 if __name__ == "__main__":
     while True:
         # 捕捉画面
         camera = cv2.VideoCapture(0)
-        _, image = camera.read()
+
+        ret, image = camera.read()
+
 
         frame = decode_display(image)
 
-        if frame is not None:
+        if frame != None:
             cv2.imshow('video', frame)
+
             cv2.waitKey(6000)
+
             color = color_detect(frame)
+            
             print(color)
 
         # 释放摄像头
