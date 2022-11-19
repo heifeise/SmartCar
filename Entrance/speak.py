eae072593c96b884f1ab49268980fdd86747af4d
吴百恒 <1243244190@qq.com>
2022-11-19 16:31:42 +0800

    更新： 在原来英文播报模式上 增加中文播报

--- Entrance/speak.py
+++ Entrance/speak.py
@@ -1,5 +1,9 @@
 import os
 
-def espaek(words):
 
-    os.system('espeak '+words)
+def espaek_english(words):
+    os.system('espeak ' + words)
+
+
+def espeak_chinese(words):
+    os.system('espeak -vzh+f2 ' + words)
