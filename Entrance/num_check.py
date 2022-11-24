7947e3f5fe66974ed5a7ee2b6c079aa2e49facd2
吴百恒 <1243244190@qq.com>
2022-11-24 14:12:02 +0800

    尝试：尝试使用百度接口进行人流量监测

--- Entrance/num_check.py
+++ Entrance/num_check.py
@@ -1,1 +1,19 @@
 # 基于百度ai人流量检测
+import requests
+import base64
+
+'''
+人流量统计（动态版）
+'''
+
+request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_tracking"
+frame = open('[本地文件]', 'rb')
+img = base64.b64encode(frame.read())
+
+params = {"area":"1,1,719,1,719,719,1,719","case_id":1,"case_init":"false","dynamic":"true","image":img}
+access_token = '[调用鉴权接口获取的token]'
+request_url = request_url + "?access_token=" + access_token
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(request_url, data=params, headers=headers)
+if response:
+    print (response.json())
