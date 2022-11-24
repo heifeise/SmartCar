ee347ed174e1675ceb2d0dcfe83fabe2ab941285
周生财 <2465315120@qq.com>
2022-11-24 15:36:48 +0800

    l port

---
+++ visual_part/ocr/Ocr.py
@@ -0,0 +1,46 @@
+# encoding:utf-8
+
+import base64
+# encoding:utf-8
+import requests
+
+
+# client_id 为官网获取的AK， client_secret 为官网获取的SK
+def gettoken():
+    token = ""
+    if token == "":
+        APP_ID = '28576885'
+        API_KEY = 'AN6IL1jnSVra26UeqXGYYzDe'
+        SECRET_KEY = 'rXssGwS1ObhuY4qK4wpTTsdH7gBNOaTL'
+
+        # client_id 为官网获取的AK， client_secret 为官网获取的SK
+        host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials' + \
+               '&client_id=' + API_KEY + \
+               '&client_secret=' + SECRET_KEY
+        # print(host)
+        response = requests.get(host)
+        # if response:
+        #     for item in response.json().items():  # 逐项遍历response.json()----字典
+        #         print(item)
+        token = response.json()["access_token"]
+        print(">>成功获取到token")
+        return token
+
+
+
+'''
+健康码识别
+'''
+
+request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/health_code"
+# 二进制方式打开图片文件
+f = open('binary.png', 'rb')
+img = base64.b64encode(f.read())
+
+params = {"image": img}
+access_token = gettoken()
+request_url = request_url + "?access_token=" + access_token
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(request_url, data=params, headers=headers)
+if response:
+    print(response.json())
